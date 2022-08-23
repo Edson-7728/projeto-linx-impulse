56e692038067c40a81c5996494dbfa77a8b9767d
Edson Vieira <elvieira.275@gmail.com>
2022-08-23 10:54:53 -0300

    criada feature de use effect e state

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import Produto from '../Produto/Produto'
-import ButtonMain from './ButtonMain'
 import './Main.css'
-import {useState} from 'react'
-import {useEffect} from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const lista = []
 const Main = () => {
@@ -15,11 +14,35 @@ const Main = () => {
         const json = await response.json()
         lista.push(json.products)
         setProdutos(lista)
+        console.log(json);
     }
-    
+
+    useEffect(() => {
+        handleBusca()
+    }, [pg])
+
     return (
-        
+        <div>
+            <h4>Sua seleção especial</h4>
+            <div>
+                <div>
+                    {produtos.map((quadro, index) => {
+                        return(
+                            <div key={index}>
+                                {quadro.map((produtos) => {
+                                    return(
+                                        <Produto key={produtos.id} fotoPrd={produtos.image} nome={produtos.name} sobre={produtos.description} preco1={produtos.oldPrice} preco2={produtos.price} xParc={produtos.installments.count} valParc={produtos.installments.value} />
+                                    )
+                                })}
+                            </div>   
+                        )
+                    })}
+                </div>
+            </div>
+            <button className='btnMais' onClick={() => {setPg (pg + 1)}}>Ainda mais produtos aqui!</button>
+            <h4>Compartilhe a novidade</h4>
+        </div>
     )
 }
 
-export default Main
+            export default Main
